12f0145df3656d85ffaf4b05fc218755f30eab73
Androwis Abumoussa <androwis@androw.is>
2013-12-07 11:25:51 -0500

    working mapping

--- src/DDiMAP.cpp
+++ src/DDiMAP.cpp
@@ -25,14 +25,14 @@ using namespace std;
 // char *file = "data/Burack_128F/128F_Gen1_Frag_WithBcl2Sanger_sorted.bam";
 string file  = "data/128test_Gen1_example_sorted.bam";
 
-void func ( map<string , map<int, map<string, int> > > reads,  void (*f)(string, int, string, int) );
-void func ( map<string , map<int, map<string, int> > > reads,  void (*f)(string, int, string, int) )
+void func ( map<int , map<int, map<string, int> > > reads,  void (*f)(int, int, string, int) );
+void func ( map<int , map<int, map<string, int> > > reads,  void (*f)(int, int, string, int) )
 {
 
 	long count 	 = 0;
 	long uniques = 0;
 
-	map<string , map<int, map<string, int> > >::iterator genes = reads.begin();
+	map<int , map<int, map<string, int> > >::iterator genes = reads.begin();
 	for(; genes != reads.end(); ++genes)
 	{
 		map<int, map<string, int> >::iterator ROAs = (*genes).second.begin();
@@ -56,10 +56,10 @@ void func ( map<string , map<int, map<string, int> > > reads,  void (*f)(string,
 }
 
 
-void print (string gene, int roa, string seq, int count){
+void print (int gene, int roa, string seq, int count){
 
 	// Print [gene][ROA][COUNT][Seq]
-	if((count) > 20)
+	if(count > 2 && gene < 10)
 		cout << gene << " [" << roa << "][" << count << "] "  << seq << endl;
 
 }
@@ -70,17 +70,16 @@ void read(){
 	BamReader *br = new BamReader();
 	BamAlignment ba;
 
-	// [gene-name [ roa [ seq count ] ] ]
-	map<string , map<int, map<string, int> > > reads;
+	// [ gene-name [ roa [ seq count ] ] ]
+	map<int , map<int, map<string, int> > > reads;
 	br->Open(file);
-
 
 	while(br->GetNextAlignment(ba))
 		if(ba.Position > 0)
 		{
 
-			string word, name;
-			name =  "test";
+			string word;
+			int name 	 =  ba.RefID;
 			int position = ba.Position;
 			int offset	 = 0;
 
@@ -97,19 +96,16 @@ void read(){
 		}
 
 	func(reads, print);
-
 }
 
 
 int main (int argc, char **argv) {
 
 	// ------------------------------------------------------------------------
-	// Paramaters
+	// Parameters
 	// ------------------------------------------------------------------------
 	int c;
-	int digit_optind = 0;
-	int aopt = 0, bopt = 0;
-	char *copt = 0, *dopt = 0;
+
 	static struct option long_options[] = {
 			{"file", 	0, 0, 'f'},
 			{NULL, 		0, NULL, 0}
@@ -117,8 +113,6 @@ int main (int argc, char **argv) {
 
 	int option_index = 0;
 	while ((c = getopt_long(argc, argv, "f:", long_options, &option_index)) != -1) {
-
-		int this_option_optind = optind ? optind : 1;
 
 		switch (c) {
 		case 'f':
@@ -141,13 +135,15 @@ int main (int argc, char **argv) {
 	// DDiMAP
 	// ------------------------------------------------------------------------
 	clock_t t;
-	int f;
 	t = clock();
 	read();
 	t = clock() - t;
 	printf ("It took me %d ticks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
 
 
+	// ------------------------------------------------------------------------
+	// End. 
+	// ------------------------------------------------------------------------
 	return EXIT_SUCCESS;
 
 }
