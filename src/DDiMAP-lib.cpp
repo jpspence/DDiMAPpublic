0163c91005c68a4eecd9bba7f03f14ff355db566
Androwis Abumoussa <androwis@androw.is>
2014-02-04 10:04:14 -0500

    assigning each read to only one track
    Former-commit-id: 924718435e6eb39bc51cbd901ad15bd429a0f461

--- src/DDiMAP-lib.cpp
+++ src/DDiMAP-lib.cpp
@@ -43,26 +43,6 @@ map<string , map<int, map<int, int> > > threshold_histogram_1;
 // Map <Reference Name -> < position -> sequence>
 map<string, map<int, uint64_t > > references;
 
-// ----------------------------------------------------------------------------
-// Convenience methods.
-// ----------------------------------------------------------------------------
-
-string createWordString(BamAlignment &ba, int length, int &position, int track)
-{
-    int to_roa    = ( length/2 - ba.Position ) % (length/2);
-    if(to_roa < 0) to_roa+=length/2;
-    int offset    = (ba.IsReverseStrand()) ? ba.AlignedBases.length() - ( (ba.Position + ba.AlignedBases.length())%(length/2)) - length  : to_roa ;
-
-    position = ba.Position + offset + track * (ba.IsReverseStrand() ? - 8 : 8);
-    string word   = ba.AlignedBases.substr(offset, length);
-    return word;
-}
-
-const char *createWordArray(BamAlignment &ba, int length, int &position, int track)
-{
-	string word = createWordString(ba, length, position, track);
-	return word.c_str();
-}
 
 // ----------------------------------------------------------------------------
 // BAM --> Reads
@@ -80,17 +60,17 @@ uint64_t charToUINT64(char ch)
 	switch(ch){
 	case 'A':
 	case 'a': return a;
-	
-  case 'T':
+
+	case 'T':
 	case 't': return t;
-	
-  case 'C':
+
+	case 'C':
 	case 'c':	return c;
-	
-  case 'G':
+
+	case 'G':
 	case 'g':	return g;
 
-  case '-': return dash;
+	case '-': return dash;
 
 	default : return 0;
 	}
@@ -151,45 +131,63 @@ Read convert(string &word, int length)
 }
 
 // ----------------------------------------------------------------------------
+// Reading Files : Convenience Functions
+// ----------------------------------------------------------------------------
+
+// We assign each read to only 1 Track
+string createWordString(BamAlignment &ba, int length, int &position)
+{
+	int to_roa    = ( length/4 - ba.Position ) % (length/4);
+	if(to_roa < 0) to_roa+=length/4;
+	int offset    = (ba.IsReverseStrand()) ? ba.AlignedBases.length() - ( (ba.Position + ba.AlignedBases.length())%(length/4)) - length  : to_roa ;
+	position = ba.Position + offset;
+	if(ba.Position < 50)
+	//cout << (ba.IsReverseStrand() ? "reverse " : "forward ") << "[" << ba.Position << " - " << (ba.Position+ba.AlignedBases.length()) << "] @" << position << endl;
+	string word   = ba.AlignedBases.substr(offset, length);
+	return word;
+}
+
+const char *createWordArray(BamAlignment &ba, int length, int &position)
+{
+	string word = createWordString(ba, length, position);
+	return word.c_str();
+}
+
+// ----------------------------------------------------------------------------
 // Reading Files
 // ----------------------------------------------------------------------------
-int counters =0;
+
 int reduce( BamAlignment &ba, int length, Read (*f)(string &, int) )
 {
-
-	int unique = 0;
-  if(ba.Position > 0)
+	if(ba.Position > 0)
 	{
-    for (int track = 0; track < 2; track ++ ){
-      int position;
-      string word   = createWordString(ba, length, position, track);
-      string name   = genes[ba.RefID];
+		int position;
+		string word   = createWordString(ba, length, position);
+		string name   = genes[ba.RefID];
 
-      // Increment counter for the observed sequence
-      if(reads[name][position][word].count)
-        reads[name][position][word].count+=1;
-      
-      // Create a new read for the position on this track
-      else {
-        Read r;
-        r = f(word, length);
+		// Increment counter for the observed sequence
+		if(reads[name][position][word].count)
+			reads[name][position][word].count+=1;
 
-        if(ba.CigarData[0].Length == 50)
-          r.set_no_indels();
+		// Create a new read for the position on this track
+		else {
+			Read r;
+			r = f(word, length);
 
-        if(references[genes_names[ba.RefID]][position] == r.left_sequence_half)
-          r.set_matches_ref_on_left();
-        
-        if(references[genes_names[ba.RefID]][position+length/2] == r.right_sequence_half)
-          r.set_matches_ref_on_right();
+			if(ba.CigarData[0].Length == 50)
+				r.set_no_indels();
 
-        reads[name][position][word] = r;
-        unique++;
-      }
-    }
-  }
+			if(references[genes_names[ba.RefID]][position] == r.left_sequence_half)
+				r.set_matches_ref_on_left();
 
-	return unique;
+			if(references[genes_names[ba.RefID]][position+length/2] == r.right_sequence_half)
+				r.set_matches_ref_on_right();
+
+			reads[name][position][word] = r;
+			return 1;
+		}
+	}
+	return 0;
 }
 
 // Returns the number of unique reads in the file.
@@ -317,55 +315,55 @@ int print (string gene, int position, string seq, Read& read)
 int verify ( string gene, int position, string seq, Read& read)
 {
 
-  map<string, Read> roaVerifier;
+	map<string, Read> roaVerifier;
 
-  // Verify the left
-  if((roaVerifier = reads[gene][position - seq.length()/2 ]).size() > 0) 
-  {
-    map<string, Read>::iterator sequences = roaVerifier.begin();
-    for (; sequences != roaVerifier.end(); ++sequences)
-      if(read.left_sequence_half == (*sequences).second.right_sequence_half &&
-        (*sequences).second.count > THRESHOLD ) 
-      {
-        read.set_left_verified();
-        break;
-      }
-  }
-  
-  // Verify the frequency
-  double count = 0;
-  if((roaVerifier = reads[gene][position]).size() > 0) 
-  {
-    map<string, Read>::iterator sequences = roaVerifier.begin();
-    for (; sequences != roaVerifier.end(); ++sequences)
-      count+=(*sequences).second.count;
-  }
-  if( (double)read.count / count > PPM)
-  read.set_above_ppm_threshold();
+	// Verify the left
+	if((roaVerifier = reads[gene][position - seq.length()/2 ]).size() > 0)
+	{
+		map<string, Read>::iterator sequences = roaVerifier.begin();
+		for (; sequences != roaVerifier.end(); ++sequences)
+			if(read.left_sequence_half == (*sequences).second.right_sequence_half &&
+					(*sequences).second.count > THRESHOLD )
+			{
+				read.set_left_verified();
+				break;
+			}
+	}
+
+	// Verify the frequency
+	double count = 0;
+	if((roaVerifier = reads[gene][position]).size() > 0)
+	{
+		map<string, Read>::iterator sequences = roaVerifier.begin();
+		for (; sequences != roaVerifier.end(); ++sequences)
+			count+=(*sequences).second.count;
+	}
+	if( (double)read.count / count > PPM)
+		read.set_above_ppm_threshold();
 
 
-  // Verify the right
-  if((roaVerifier = reads[gene][position + seq.length()/2 ]).size() > 0)
-  {
-    map<string, Read>::iterator sequences = roaVerifier.begin();
-    for (; sequences != roaVerifier.end(); ++sequences)
-      if(read.right_sequence_half == (*sequences).second.left_sequence_half &&
-         (*sequences).second.count > THRESHOLD )
-      {
-        read.set_right_verified();
-        break;
-      }
-  }
+	// Verify the right
+	if((roaVerifier = reads[gene][position + seq.length()/2 ]).size() > 0)
+	{
+		map<string, Read>::iterator sequences = roaVerifier.begin();
+		for (; sequences != roaVerifier.end(); ++sequences)
+			if(read.right_sequence_half == (*sequences).second.left_sequence_half &&
+					(*sequences).second.count > THRESHOLD )
+			{
+				read.set_right_verified();
+				break;
+			}
+	}
 
-  // Print the verified words that are not reference.
-//  if( not read.matches_ref_on_left_and_right() && read.is_right_left_verified() )
-//    cout << gene << " : " << roa << " : " << seq << " is verified and unique." << read.is_right_left_verified() << endl;
+	// Print the verified words that are not reference.
+	//  if( not read.matches_ref_on_left_and_right() && read.is_right_left_verified() )
+	//    cout << gene << " : " << roa << " : " << seq << " is verified and unique." << read.is_right_left_verified() << endl;
 
-  if( read.is_right_left_verified() && 
-      not read.matches_reference() )
-    return 1;
+	if( read.is_right_left_verified() &&
+			not read.matches_reference() )
+		return 1;
 
-  return 0;
+	return 0;
 
 }
 
@@ -373,31 +371,42 @@ int verify ( string gene, int position, string seq, Read& read)
 int buildHistograms(string gene, int position, string seq, Read& read)
 {
 
-  if( ( read.is_right_left_verified() || read.is_above_ppm() ) && 
-      not read.matches_reference() )
+	map<string, map <int, map<int,int> > > * verified;
+	map<string, map <int, map<int,int> > > * frequency;
+
+	if( position % ROA_LENGTH == 0 || position % ROA_LENGTH == ROA_LENGTH/2){
+		verified  =  &verified_histogram_0;
+		frequency = &threshold_histogram_0;
+	} else {
+		verified  =  &verified_histogram_1;
+		frequency = &threshold_histogram_1;
+	}
+
+	if( ( read.is_right_left_verified() || read.is_above_ppm() ) &&
+			not read.matches_reference() )
 	{
-    
+
 		// Create a histogram for the left half
 		uint64_t s = read.left_sequence_half;
 		int i = 0;
 		while(s!=0)
 		{
-      if( read.is_right_left_verified() )
-    	  verified_histogram_0[gene][position + i][ s & 0b00000111] += read.count;
-      if( read.is_above_ppm() )
-    	  threshold_histogram_0[gene][position + i][ s & 0b00000111] += read.count;
-		  s = s>>3;i++;
+			if( read.is_right_left_verified() )
+				(*verified)[gene][position + i][ s & 0b00000111] += read.count;
+			if( read.is_above_ppm() )
+				(*frequency)[gene][position + i][ s & 0b00000111] += read.count;
+			s = s>>3;i++;
 		}
 
 		// Then the right
 		s = read.right_sequence_half;
 		while(s!=0)
-    {
-      if( read.is_right_left_verified() )
-    	  verified_histogram_0[gene][position + i][ s & 0b00000111] += read.count;
-      if( read.is_above_ppm() )
-    	  threshold_histogram_0[gene][position + i][ s & 0b00000111] += read.count;
-			  s=s>>3;i++;
+		{
+			if( read.is_right_left_verified() )
+				(*verified)[gene][position + i][ s & 0b00000111] += read.count;
+			if( read.is_above_ppm() )
+				(*frequency)[gene][position + i][ s & 0b00000111] += read.count;
+			s=s>>3;i++;
 		}
 
 	}
@@ -428,4 +437,12 @@ void printHistograms()
 
 		f.close();fc.close();
 	}
+}
+
+
+void callSNVs()
+{
+
+	// For each gene
+	// Look at both start positions and
 }
